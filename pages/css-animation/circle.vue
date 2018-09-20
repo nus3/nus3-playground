09b3efc018dbd96eabcb5448d67a7c37326c2f65
yota-hada <hadayota33@gmail.com>
2018-09-20 22:31:32 +0900

    feat:  move to front and back.

---
+++ pages/css-animation/circle.vue
@@ -0,0 +1,54 @@
+<template>
+    <main class="wrapper">
+        <div class="container">
+            <div class="circle"></div>
+        </div>
+    </main>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+    position: relative;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #FFF8E1;
+
+    .container {
+        width: 80%;
+        height: 80%;
+        position: relative;
+        // border: solid 1px black;
+
+        .circle {
+            position: absolute;
+            width: 50px;
+            height: 50px;
+            border-radius: 50%;
+            background-color: #ffb300;
+            animation: moveForward 3s ease-in infinite;
+        }
+    }
+}
+
+
+@keyframes moveForward {
+    0% {
+        left: 0;
+    }
+    50% {
+        left: 100%;
+    }
+    100% {
+        left: 0;
+    }
+}
+
+</style>
+
---
+++ pages/css-animation/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <section class="container">
+        <h1>cssアニメーションのまとめ</h1>
+        <ul>
+            <li>
+                <nuxt-link :to="'/'">HOMEへ</nuxt-link>
+            </li>
+            <li>
+                <nuxt-link :to="'/css-animation/circle'">円のやつ</nuxt-link>
+            </li>
+        </ul>
+    </section>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
