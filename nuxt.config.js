67294ac9e4df5ee80a52e74fca574c6874e9f862
yota-hada <hadayota33@gmail.com>
2019-01-21 22:45:28 +0900

    feat: add ogp.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  mode: 'universal',
+
   /*
   ** Headers of the page
   */
@@ -7,10 +9,20 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Nus3の実験場。CSSアニメーションで遊ぶ場所' },
+      { hid: 'keywords', name: 'keywords', content: 'CSS,Animation,アニメーション' },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'Nus3-Playground' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:title', property: 'og:title', content: 'Nus3-Playground' },
+      { hid: 'og:image', property: 'og:image', content: '/image/logo.png' },
+      { name: 'twitter:title', content: 'Nus3-Playground' },
+      { name: 'twitter:site', content: '@YotaHada3' },
+      { name: 'twitter:image', content: '/image/facebook_cover_photo_2.png' },
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:description', content: 'アニメーションで遊ぶ場所' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/png', href: '/image/favicon.png' },
       // { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css' },
     ],
     script: [
